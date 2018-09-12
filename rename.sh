f821befaac6686554092adbeb2b145eb6c4e5f9a
Radu Suciu <radusuciu@gmail.com>
2018-09-12 09:01:50 -0700

    Ignore case when renaming project

--- rename.sh
+++ rename.sh
@@ -6,7 +6,7 @@ new_project_name="${1}"
 default_project_name='quickvue'
 project_path=$(dirname "$(readlink -f "$0")")
 
-grep -rl --exclude-dir=.git "${default_project_name}" "${project_path}" | xargs sed -i "s/${default_project_name}/${new_project_name}/g"
+grep -rli --exclude-dir=.git "${default_project_name}" "${project_path}" | xargs sed -i "s/${default_project_name}/${new_project_name}/gI"
 
 # change name of package directory
 cd "${project_path}" || { echo "Could not change directories to project path"; exit 1; }
