737f144212416f11cf978305fd86e21c22e3cb83
Radu Suciu <radusuciu@gmail.com>
2018-09-12 09:48:18 -0700

    Do not hardcode python 3.5

--- start.sh
+++ start.sh
@@ -9,7 +9,7 @@ export FLASK_APP='quickvue/__init__.py'
 
 # this is pretty specific to my system running Linux Subsystem for Windows with conda
 if [ ! -d venv ]; then
-    python3.5 -m venv --without-pip venv
+    python3 -m venv --without-pip venv
     source venv/bin/activate
     curl https://bootstrap.pypa.io/get-pip.py | python
     deactivate
