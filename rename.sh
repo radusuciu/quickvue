8d6e5a67eba057902d52dec4cdb665d82dc582ca
Radu Suciu <radusuciu@gmail.com>
2018-09-11 22:23:07 -0700

    Fixes to rename script.

--- rename.sh
+++ rename.sh
@@ -3,12 +3,8 @@
 new_project_name="${1}"
 [[ -z "${new_project_name}" ]] && { echo "New project name is empty"; exit 1; }
 
-default_project_name='inhibition_ratiobar'
+default_project_name='quickvue'
 project_path=$(dirname "$(readlink -f "$0")")
-new_project_path=$(dirname "${project_path}")/"${new_project_name}"
-
-echo ${project_path}
-echo ${new_project_path}
 
 grep -rl --exclude-dir=.git "${default_project_name}" "${project_path}" | xargs sed -i "s/${default_project_name}/${new_project_name}/g"
 
@@ -17,5 +13,5 @@ cd $(dirname "${project_path}") || { echo "Could not change directories to proje
 mv "${default_project_name}" "${new_project_name}"
 
 # change name of package directory
-cd "${new_project_path}" || { echo "Could not change directories to new project path"; exit 1; }
+cd "${new_project_name}" || { echo "Could not change directories to new project path"; exit 1; }
 mv "${default_project_name}" "${new_project_name}"
