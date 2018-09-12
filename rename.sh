4dee6fccb12c6715d4c058b32faf1d2f2949daa4
Radu Suciu <radusuciu@gmail.com>
2018-09-11 23:03:48 -0700

    Don't rename parent folder.. had issues with this.

--- rename.sh
+++ rename.sh
@@ -8,10 +8,6 @@ project_path=$(dirname "$(readlink -f "$0")")
 
 grep -rl --exclude-dir=.git "${default_project_name}" "${project_path}" | xargs sed -i "s/${default_project_name}/${new_project_name}/g"
 
-# change name of project directory  
-cd $(dirname "${project_path}") || { echo "Could not change directories to project parent path"; exit 1; }
-mv "${default_project_name}" "${new_project_name}"
-
 # change name of package directory
-cd "${new_project_name}" || { echo "Could not change directories to new project path"; exit 1; }
+cd "${project_path}" || { echo "Could not change directories to project path"; exit 1; }
 mv "${default_project_name}" "${new_project_name}"
